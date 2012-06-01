3a6eb8bd7caa1a018a740e8400a619e0c6d208dc
Nick Jalbert <nickj@captricity.com>
2012-05-31 20:15:05 -0700

    transition to list of docs

--- src/com/example/whiskeydroid/DummyLoginActivity.java
+++ src/com/example/whiskeydroid/DummyLoginActivity.java
@@ -1,31 +1,76 @@
 package com.example.whiskeydroid;
 
+import java.util.ArrayList;
+
 import android.animation.AnimatorInflater;
 import android.animation.ObjectAnimator;
 import android.app.Activity;
+import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.animation.AccelerateInterpolator;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 
-public class DummyLoginActivity extends Activity {
 
+/* get your flip on
+ * http://www.inter-fuser.com/2009/08/android-animations-3d-flip.html
+ */
+public class DummyLoginActivity extends ListActivity implements CaptricityResultReceiver.Receiver {
 	private View login_view;
 	private View dark_side;
 	private boolean isLoginView = true;
+	public CaptricityResultReceiver mReceiver;
+	public ArrayAdapter<DocumentData> adapter;
+	ArrayList<DocumentData> listItems = new ArrayList<DocumentData>();
 
+	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
+		mReceiver = new CaptricityResultReceiver(new Handler());
+		mReceiver.setReceiver(this);
+	    
 		super.onCreate(savedInstanceState);
+	
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.dummylogin);
 		login_view = findViewById(R.id.ll);
 		dark_side = findViewById(R.id.dsl);
 		dark_side.setVisibility(View.GONE);
+	
 		createSignInButton();
+		
+		adapter = new ArrayAdapter<DocumentData>(this, android.R.layout.simple_list_item_1, listItems);
+		setListAdapter(adapter);
+	    final Intent intent = new Intent(Intent.ACTION_SYNC, null, this, QueryCaptricityAPI.class);
+	    intent.putExtra(QueryCaptricityAPI.receiverKey, mReceiver);
+	    intent.putExtra(QueryCaptricityAPI.commandKey, QueryCaptricityAPI.listDocsCommand);
+	    startService(intent);
+	}
+	
+	@Override
+	protected void onListItemClick(ListView l, View v, int position, long id) {
+		DocumentData item = listItems.get(position);
+		Log.w("LDA", "You clicked " + item + "!");
+		Intent docDetailsIntent = new Intent(v.getContext(), DocumentDetailsActivity.class);
+		docDetailsIntent.putExtra(DocumentDetailsActivity.document_data_key, item);
+        startActivity(docDetailsIntent);
+	}
+
+	public void onReceiveResult(int resultCode, Bundle resultData) {
+		if (resultCode == QueryCaptricityAPI.FINISHED) {
+			ArrayList<DocumentData> results = resultData.getParcelableArrayList(QueryCaptricityAPI.resultKey);
+			listItems.clear();
+			for (DocumentData result:results) {
+				listItems.add(result);
+			}
+			adapter.notifyDataSetChanged();
+		}
 	}
 
 	private void createSignInButton() {
