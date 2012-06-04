61f3ce4c1c317d2e4187d684b96216c2cd1301b6
Nick Jalbert <nickj@captricity.com>
2012-06-04 01:33:27 -0700

    checkpoint

--- src/com/example/whiskeydroid/JobDetailsActivity.java
+++ src/com/example/whiskeydroid/JobDetailsActivity.java
@@ -2,6 +2,7 @@ package com.example.whiskeydroid;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Window;
 import android.widget.TextView;
 
 public class JobDetailsActivity extends Activity {
@@ -11,6 +12,7 @@ public class JobDetailsActivity extends Activity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
     	setContentView(R.layout.jobdetails);
 	    setJob();
     	TextView job_name = (TextView) findViewById(R.id.job_details_name);
